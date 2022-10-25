960ce1e2ecdd617dedfb917f4fb816753d439260
Darina <darina.blazevica@accenture.com>
2022-10-25 10:21:45 +0300

    Topnavigation implementation

---
+++ sitecore/definitions/components/topnavigation.sitecore.ts
@@ -0,0 +1,18 @@
+import { CommonFieldTypes, SitecoreIcon, Manifest } from '@sitecore-jss/sitecore-jss-dev-tools';
+
+/**
+ * Adds the topnavigation component to the disconnected manifest.
+ * This function is invoked by convention (*.sitecore.ts) when `jss manifest` is run.
+ */
+export default function topnavigation(manifest: Manifest) {
+  manifest.addComponent({
+    name: 'Topnavigation',
+    icon: SitecoreIcon.DocumentTag,
+    fields: [
+      { name: 'heading', type: CommonFieldTypes.RichText},
+      { name: 'shopping_cart', type: CommonFieldTypes.Image},
+      { name: 'hamburger', type: CommonFieldTypes.Image},
+
+    ],
+  });
+}
